045c9e20d83b3b2839ee97538e5c8fd5e1fb0991
EIKO7 <camper1773@mail.ru>
2026-06-13 14:36:10 +0300

    ver 3: erase_value and sort methods

--- ahrameev.vladislav/S3/vector.h
+++ ahrameev.vladislav/S3/vector.h
@@ -73,6 +73,17 @@ public:
         return false;
     }
 
+    bool erase_value(const T& v) {
+        for (size_t i = 0; i < size_; ++i) {
+            if (data_[i] == v) {
+                for (size_t j = i; j < size_ - 1; ++j) data_[j] = std::move(data_[j + 1]);
+                --size_;
+                return true;
+            }
+        }
+        return false;
+    }
+
     T& operator[](size_t i) { return data_[i]; }
     const T& operator[](size_t i) const { return data_[i]; }
     size_t size() const { return size_; }
@@ -82,7 +93,9 @@ public:
     T* end() { return data_ + size_; }
     const T* begin() const { return data_; }
     const T* end() const { return data_ + size_; }
+
+    void sort() { if (size_ > 1) std::sort(data_, data_ + size_); }
 };
 
 } 
-#endif 
+#endif
