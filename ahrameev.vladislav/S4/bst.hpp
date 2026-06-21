cf9e60fb2f6aaea33f9a38c0dee817d421e7cdd4
EIKO7 <camper1773@mail.ru>
2026-06-22 02:40:17 +0300

    Add BSTree base structure with fake-leaf

--- ahrameev.vladislav/S4/bst.hpp
+++ ahrameev.vladislav/S4/bst.hpp
@@ -3,6 +3,7 @@
 
 #include <cstddef>
 #include <utility>
+#include <functional>
 
 namespace ahrameev
 {
@@ -172,6 +173,144 @@ public:
   bool operator!=(const BSTConstIterator& other) const
   {
     return current_ != other.current_;
+  }
+};
+
+template< class Key, class Value, class Compare = std::less< Key > >
+class BSTree
+{
+  using Node = BSTNode< Key, Value >;
+
+  Node* root_;
+  Node* sentinel_;
+  size_t size_;
+  Compare comp_;
+
+  void clear(Node* node)
+  {
+    if (node == sentinel_) {
+      return;
+    }
+    clear(node->left);
+    clear(node->right);
+    delete node;
+  }
+
+  Node* copy(Node* node, Node* parent)
+  {
+    if (node == sentinel_) {
+      return sentinel_;
+    }
+    Node* newNode = new Node(node->data.first, node->data.second, parent);
+    newNode->left = copy(node->left, newNode);
+    newNode->right = copy(node->right, newNode);
+    return newNode;
+  }
+
+public:
+  using iterator = BSTIterator< Key, Value >;
+  using const_iterator = BSTConstIterator< Key, Value >;
+
+  BSTree()
+    : root_(nullptr)
+    , sentinel_(nullptr)
+    , size_(0)
+    , comp_()
+  {
+    sentinel_ = new Node();
+    sentinel_->left = sentinel_;
+    sentinel_->right = sentinel_;
+    sentinel_->parent = sentinel_;
+    root_ = sentinel_;
+  }
+
+  BSTree(const BSTree& other)
+    : root_(nullptr)
+    , sentinel_(nullptr)
+    , size_(0)
+    , comp_(other.comp_)
+  {
+    sentinel_ = new Node();
+    sentinel_->left = sentinel_;
+    sentinel_->right = sentinel_;
+    sentinel_->parent = sentinel_;
+    if (other.root_ != other.sentinel_) {
+      root_ = copy(other.root_, sentinel_);
+      size_ = other.size_;
+    } else {
+      root_ = sentinel_;
+    }
+  }
+
+  BSTree& operator=(const BSTree& other)
+  {
+    if (this != &other) {
+      clear(root_);
+      delete sentinel_;
+      sentinel_ = new Node();
+      sentinel_->left = sentinel_;
+      sentinel_->right = sentinel_;
+      sentinel_->parent = sentinel_;
+      comp_ = other.comp_;
+      if (other.root_ != other.sentinel_) {
+        root_ = copy(other.root_, sentinel_);
+        size_ = other.size_;
+      } else {
+        root_ = sentinel_;
+        size_ = 0;
+      }
+    }
+    return *this;
+  }
+
+  ~BSTree()
+  {
+    clear(root_);
+    delete sentinel_;
+  }
+
+  iterator begin()
+  {
+    if (root_ == sentinel_) {
+      return end();
+    }
+    Node* node = root_;
+    while (node->left != sentinel_) {
+      node = node->left;
+    }
+    return iterator(node, sentinel_);
+  }
+
+  iterator end()
+  {
+    return iterator(sentinel_, sentinel_);
+  }
+
+  const_iterator begin() const
+  {
+    if (root_ == sentinel_) {
+      return end();
+    }
+    const Node* node = root_;
+    while (node->left != sentinel_) {
+      node = node->left;
+    }
+    return const_iterator(node, sentinel_);
+  }
+
+  const_iterator end() const
+  {
+    return const_iterator(sentinel_, sentinel_);
+  }
+
+  bool empty() const
+  {
+    return root_ == sentinel_;
+  }
+
+  size_t size() const
+  {
+    return size_;
   }
 };
 
