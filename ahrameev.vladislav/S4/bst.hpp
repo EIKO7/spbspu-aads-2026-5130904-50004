544ad27ec617724c78ac6dedf4551bb63a9559a9
EIKO7 <camper1773@mail.ru>
2026-06-22 02:34:48 +0300

    Add BSTIterator with increment operators

--- ahrameev.vladislav/S4/bst.hpp
+++ ahrameev.vladislav/S4/bst.hpp
@@ -30,6 +30,76 @@ struct BSTNode
   {}
 };
 
-} // namespace ahrameev
+template< class Key, class Value >
+class BSTIterator
+{
+  template< class K, class V, class C >
+  friend class BSTree;
 
-#endif // BST_HPP
+  using Node = BSTNode< Key, Value >;
+
+  Node* current_;
+  Node* sentinel_;
+
+public:
+  BSTIterator()
+    : current_(nullptr)
+    , sentinel_(nullptr)
+  {}
+
+  BSTIterator(Node* node, Node* sentinel)
+    : current_(node)
+    , sentinel_(sentinel)
+  {}
+
+  std::pair< Key, Value >& operator*() const
+  {
+    return current_->data;
+  }
+
+  std::pair< Key, Value >* operator->() const
+  {
+    return &(current_->data);
+  }
+
+  BSTIterator& operator++()
+  {
+    if (current_->right != sentinel_) {
+      current_ = current_->right;
+      while (current_->left != sentinel_) {
+        current_ = current_->left;
+      }
+    } else {
+      Node* p = current_->parent;
+      while (p != sentinel_ && current_ == p->right) {
+        current_ = p;
+        p = p->parent;
+      }
+      if (p != sentinel_ || current_ == sentinel_) {
+        current_ = p;
+      }
+    }
+    return *this;
+  }
+
+  BSTIterator operator++(int)
+  {
+    BSTIterator tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+
+  bool operator==(const BSTIterator& other) const
+  {
+    return current_ == other.current_;
+  }
+
+  bool operator!=(const BSTIterator& other) const
+  {
+    return current_ != other.current_;
+  }
+};
+
+} 
+
+#endif 
