c194520238a7228d726c22d850776b7db194ff10
EIKO7 <camper1773@mail.ru>
2026-06-22 20:15:14 +0300

    Add dict.hpp with Dictionary type aliases

---
+++ ahrameev.vladislav/S4/dict.hpp
@@ -0,0 +1,16 @@
+#ifndef DICT_HPP
+#define DICT_HPP
+
+#include <string>
+
+#include "bst.hpp"
+
+namespace ahrameev
+{
+
+using Dictionary = BSTree< int, std::string >;
+using DictionaryStore = BSTree< std::string, Dictionary >;
+
+}
+
+#endif 
