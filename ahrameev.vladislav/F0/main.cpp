6e77973d0518b4392a79a2fdb149650ef0d442cc
EIKO7 <camper1773@mail.ru>
2026-06-27 01:04:57 +0300

    basic project skeleton and input loop

--- ahrameev.vladislav/F0/main.cpp
+++ ahrameev.vladislav/F0/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
-    
+int main() {
+  std::cin.tie(nullptr);
+  std::ios_base::sync_with_stdio(false);
+
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    if (line.empty()) {
+      continue;
+    }
+    std::cout << "<OK>\n";
+  }
 }
